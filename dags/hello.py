ed357250306983b12cb63d8da3cacfaeec7ad502
Vinicius Henrique dos Santos <vinicius-dossantos@outlook.com>
2025-01-15 16:37:42 -0300

    att

--- dags/hello.py
+++ dags/hello.py
@@ -1,28 +1,29 @@
-
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
 default_args = {
-    'owner': 'datamasterylab',
-    'start_date': datetime(year=2025, month=1, day=15),
+    'owner': 'datamasterylab.com',
+    'start_date': datetime(2024, 1, 25),
     'catchup': False
 }
 
 dag = DAG(
-    dag_id='hello_world',
-    default_args=default_args,
+    'hello_world',
+    default_args = default_args,
     schedule=timedelta(days=1)
 )
 
 t1 = BashOperator(
-    task_id='hello_world',
+    task_id = 'hello_world',
     bash_command='echo "Hello World"',
-    dag=dag
+    dag = dag
 )
 
 t2 = BashOperator(
-    task_id='hello_dml',
+    task_id = 'hello_dml',
     bash_command='echo "Hello Data Mastery Lab"',
-    dag=dag
+    dag = dag
 )
+
+t1 >> t2
